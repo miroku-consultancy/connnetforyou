2490734c6790a10ff3ef82d3eb57a39994fc6b71
avinashkr1 <avinashkr1@hotmail.com>
2025-06-25 16:04:00 +0530

    Deploy: latest React build added to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { CartProvider } from './components/CartContext';
 import { UserProvider } from './components/UserContext';
 import EmailTokenLogin from './components/EmailTokenLogin';
