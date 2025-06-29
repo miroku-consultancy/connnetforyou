6d4b81f1cc6fade6615b00225a17250cce3f039e
avinashkr1 <avinashkr1@hotmail.com>
2025-06-29 21:42:46 +0530

    Shop Qr Code

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -242,7 +242,7 @@ const Product = () => {
   const freshProducts = products.filter(
     (p) => {
       const category = p.category?.toLowerCase();
-      return category === 'fresh' || category === 'sweets' || category === 'snacks' || category === 'Biryani';
+      return category === 'fresh' || category === 'sweets' || category === 'snacks' || category === 'biryani';
     }
   );
 
