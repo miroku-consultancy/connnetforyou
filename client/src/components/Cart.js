72e959f9122861d37522a7e959b61134460778b8
avinashkr1 <avinashkr1@hotmail.com>
2025-07-03 11:22:54 +0530

    logout button fixes

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -15,7 +15,7 @@ const Cart = () => {
   useEffect(() => {
     if (items.length > 0) {
       setVisible(true);
-      const timer = setTimeout(() => setVisible(false), 3000);
+      const timer = setTimeout(() => setVisible(false), 1500);
       return () => clearTimeout(timer);
     }
   }, [items.length]);
