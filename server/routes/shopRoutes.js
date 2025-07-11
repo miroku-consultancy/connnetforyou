b7fa96d06d09cd69f09ea50355ded32b2715e253
avinashkr1 <avinashkr1@hotmail.com>
2025-07-11 12:07:16 +0530

    Bug Fixes

--- server/routes/shopRoutes.js
+++ server/routes/shopRoutes.js
@@ -63,7 +63,7 @@ router.get('/', async (req, res) => {
 
   const userLat = parseFloat(lat);
   const userLng = parseFloat(lng);
-  const maxDistance = 25; // in km
+  const maxDistance = 5; // in km
 
   const getDistance = (lat1, lon1, lat2, lon2) => {
     const R = 6371;
