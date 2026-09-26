b21473793661ebcc2d3829c43933c7c930328513
jusping <connectfree4u@gmail.com>
2026-09-26 12:34:40 +0530

    Add initial services module and dashboard integration

--- server/index.js
+++ server/index.js
@@ -52,6 +52,7 @@ const allowedOrigins = [
   'https://www.connectfree4u.com',
   'https://jusping.com',
   'https://www.jusping.com',
+  ,'https://city-home-services.jusping.com',
   ];
 
 // Configure CORS middleware
